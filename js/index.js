58054308892b62e9cdd402bbebea8194e3ffeeed
Ayodeji <46384451+dej10@users.noreply.github.com>
2020-09-06 20:47:27 +0100

    Intial commit

---
+++ js/index.js
@@ -0,0 +1,30 @@
+ function dice () {
+ 	var randomNumber1, randomNumber2;
+	randomNumber1 =  Math.floor((Math.random() * 6) + 1);
+	randomNumber2 = Math.floor((Math.random() * 6) + 1);
+	 
+	document.querySelector(".dice-player1").setAttribute("src", "images/dice" + randomNumber1 + ".png" );
+	document.querySelector(".dice-player2").setAttribute("src", "images/dice" + randomNumber2 + ".png" );
+
+	if(randomNumber1 > randomNumber2){
+	document.querySelector("h1").innerHTML = "Player 1 Wins!";
+
+	}
+
+	else if(randomNumber2 > randomNumber1){
+  document.querySelector("h1").innerHTML = "Player 2 Wins!";
+
+	}
+	else{
+  document.querySelector("h1").innerHTML = "Draw! Roll Again";
+	}
+
+ }
+
+ dice();
+	
+
+
+	
+	
+
